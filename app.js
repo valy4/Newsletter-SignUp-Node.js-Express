a2f3b3f803e8eb3eae31c1d3ff5b8f20ba6b58f6
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-07-01 17:47:25 +0100

    progress

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+
 
 
 const express = require(`express`)
@@ -6,7 +6,47 @@ const bodyParser = require(`body-parser`)
 const request = require(`request`)
 
 const app = express()
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname, "/signup.html")
+})
+
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName
+  var secondName = req.body.lName
+  var email = req.body.email
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: secondName,
+
+      }
+    }]
+  };
+  var jsonData = JSON.stringify(data)
+  const url = "https://us6.api.mailchimp.com/3.0/lists/8800485fd1"
+  const options = {
+    method: "POST",
+    auth: "valy4:b0be3613219eb502b5507ab37dbe6ceb - us6"
+  }
+  https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(data)
+    })
+
+  })
+})
 
 app.listen(3000, function () {
   console.log("server is running on port 3000")
 })
+// 654db9fb049eeb5328bda2442a4acdd6 - us6
+// b0be3613219eb502b5507ab37dbe6ceb - us6
+// 8800485fd1
